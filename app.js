b6d7c752dfe0fb91eb870b3f36f85718cac28eaa
dhulliath <983915+dhulliath@users.noreply.github.com>
2018-01-17 15:54:51 -0700

    sync startup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const jsonfile = require('jsonfile')
 const randomstring = require('randomstring')
 const fs = require('fs')
+const sync = require('sync')
 
 var ojtek = {
     app: express(),
@@ -13,7 +14,7 @@ var ojtek = {
         'gitSecret': randomstring.generate(64),
         'listenPort': 3000
     },
-    loadVariables: function() {
+    loadVariables: function () {
         jsonfile.readFile(ojtek.files.variables, (err, obj) => {
             if (err) {
                 console.log('no variable file; generating')
@@ -23,38 +24,38 @@ var ojtek = {
             }
         })
     },
-    saveVariables: function() {
+    saveVariables: function () {
         jsonfile.writeFile(ojtek.files.variables, ojtek.config)
+    },
+    openServer: function () {
+        if (isNaN(parseInt(ojtek.config.listenPort))) {
+            fs.stat(ojtek.config.listenPort, function (err) {
+                if (!err) {
+                    fs.unlinkSync(ojtek.config.listenPort)
+                }
+                ojtek.app.listen(ojtek.config.listenPort, () => {
+                    fs.chmodSync(ojtek.config.listenPort, '775')
+                    console.log('server is listening on ' + ojtek.config.listenPort)
+                })
+            })
+        } else {
+            ojtek.app.listen(ojtek.config.listenPort, () => {
+                console.log('server is listening on ' + ojtek.config.listenPort)
+            })
+        }
+    },
+    loadModules: function () {
+        var normalizedPath = require("path").join(__dirname, "modules");
+        fs.readdirSync(normalizedPath).forEach(function (file) {
+            console.log('loading module: ' + file)
+            require("./modules/" + file).init(ojtek);
+        });
+    },
+    start: function () {
+        sync(ojtek.loadVariables())
+        sync(ojtek.loadModules())
+        sync(ojtek.openServer())
     }
 }
 
-//load config variables
-ojtek.loadVariables()
-
-//load modules
-var normalizedPath = require("path").join(__dirname, "modules");
-fs.readdirSync(normalizedPath).forEach(function(file) {
-    console.log('loading module: ' + file)
-    require("./modules/" + file).init(ojtek);
-});
-
-if (isNaN(parseInt(ojtek.config.listenPort))) {
-fs.stat(ojtek.config.listenPort, function(err) {
-    if (!err) {fs.unlinkSync(ojtek.config.listenPort) }
-    ojtek.app.listen(ojtek.config.listenPort, () => {
-        fs.chmodSync(ojtek.config.listenPort, '775')
-        console.log('server is listening on ' + ojtek.config.listenPort)
-    })
-})
-} else {
-    ojtek.app.listen(ojtek.config.listenPort, ()=> {
-        console.log('server is listening on ' + ojtek.config.listenPort)
-    })
-}
-
-/*if (!Number.isInteger(ojtek.config.listenPort)) {
-    fs.unlink(ojtek.config.listenPort)
-}
-ojtek.app.listen(ojtek.config.listenPort, () => {
-    console.log('server is listening')
-})*/
+ojtek.start()
